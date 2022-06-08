89d41d73c7376b35da2d35805d650c0274bff364
KramerEd <danat7531@gmail.com>
2022-06-08 22:31:25 +0300

    fix blur toggle

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ const burger = document.querySelector(".header__burger");
 const links = document.querySelector(".header__list");
 const blur = document.querySelector(".blur");
 
-burger.addEventListener("click", () => {
+const toggleMenu = () => {
 	body.classList.toggle("active");
 
 	burger.classList.toggle("active");
 	links.classList.toggle("active");
 	blur.classList.toggle("active");
+};
+
+burger.addEventListener("click", () => {
+	toggleMenu();
 });
 blur.addEventListener("click", () => {
-	body.classList.toggle("active");
-
-	burger.classList.toggle("active");
-	links.classList.toggle("active");
-	blur.classList.toggle("active");
+	if (blur.classList.contains("active")) toggleMenu();
 });
